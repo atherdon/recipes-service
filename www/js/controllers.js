ac8744642654d2e6940485de431594db0bc3891d
Arthur <arthur@netweightit.com>
2016-09-19 02:26:58 +0300

    updates

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -62,6 +62,7 @@ function ($scope, $stateParams) {
 
 
 }])
+
    
 .controller('freeRecipesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
